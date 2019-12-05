b0b85c5b69d57eaf1b1e0ac718b52cfc083914bd
Nik <nikolay0701@ukr.net>
2019-12-05 22:45:12 +0200

    task4

---
+++ lesson2/task4/Index.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+console.log('a: ', a);
+console.log('b: ', b);
+console.log('a-b: ', a - b);
+console.log('a+b: ', a + b);
+console.log('a/b: ', a / b);
+console.log('a*b: ', a * b);
