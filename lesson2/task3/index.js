05138001fe75fff82dd72bff5f290718cda30e4f
Nik <nikolay0701@ukr.net>
2019-12-05 22:22:08 +0200

    inex

---
+++ lesson2/task3/index.js
@@ -0,0 +1,10 @@
+const goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+const customer = { name: 'John', age: 17 };
+const anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer);
+console.log(anotherCustomer);
