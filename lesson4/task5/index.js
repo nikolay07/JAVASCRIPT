d6338e01b97fd0a20e7bb73715a15c51237f52eb
Nik <nikolay0701@ukr.net>
2019-12-11 01:06:39 +0200

    3

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -2,7 +2,7 @@ const m = 3;
 const n = 16;
 let result = 0;
 
-for (let i = m; i < n; i++) {
+for (let i = m; i <= n; i++) {
     if (i % 2 === 0 && i % 4 !== 0) {
         result += i;
     } else if (i % 3 === 0) {
