7a2e5bb7d2bebefc5279811ff8fd7af2efff3577
Nik <nikolay0701@ukr.net>
2019-12-12 00:57:18 +0200

    w

--- lesson5/task5/index.js
+++ lesson5/task5/index.js
@@ -1,8 +1,7 @@
 function getSum(num1, num2) {
     let sum = 0;
-    for (let i = num1; i <= num2; num1++);
-    if (num1 % 2 === 0) {
-        sum += i;
-        return sum;
+    for (let i = num1; i <= num2; i++) {
+        if (i % 2 === 0) sum += i;
     }
+    return sum;
 }
