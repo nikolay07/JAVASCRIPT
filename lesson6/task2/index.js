e1b9906df829c64dc4dba9f7492ded3a412c6e88
Nik <nikolay0701@ukr.net>
2019-12-14 00:21:26 +0200

    w

---
+++ lesson6/task2/index.js
@@ -0,0 +1,24 @@
+const numbers = []
+
+function getSum(sumNumbers) {
+    let someOfNumbers = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (!Array.isArray(sumNumbers)) {
+            return null;
+        } else {
+            someOfNumbers += numbers[i];
+            return someOfNumbers;
+        }
+    }
+}
+
+/*
+Найдите сумму элементов массива
+ 
+Создайте функцию getSum, которая будет принимать массив чисел и врнет сумму всех элементов
+
+Основные требования:
+Функция должна называться getSum
+Функция должна принимать массив чисел как аргумент
+>Функция должна вернуть сумму всех элементов массива
+Функция должна вернуть null, если в качестве аргумента передан не массив */
