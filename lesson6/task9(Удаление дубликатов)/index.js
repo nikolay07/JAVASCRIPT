cedbe75c9a6f63ae2de1fb6f944782b06e66dbc1
Nik <nikolay0701@ukr.net>
2019-12-15 17:13:47 +0200

    1

--- lesson6/task9(Удаление дубликатов)/index.js
+++ lesson6/task9(Удаление дубликатов)/index.js
@@ -1,3 +1,21 @@
+function removeDuplicates(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    let newArray = [];
+    for (let i of array) {
+        if (!newArray.includes(array[i]) < 0) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray.length > 0 ? newArray.length : null;
+
+}
+
+
+
+
+
 /*Удаление дубликатов
 Напишите функцию removeDuplicates, которая будет удалять дубликаты с входящего массива и возвращать новый массив. Длина массива > 0
 Сигнатура - removeDuplicates(array)
