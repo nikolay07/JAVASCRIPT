3982c2c22ba583ad24417b22ecc9945589a88ba4
Nik <nikolay0701@ukr.net>
2019-12-15 17:54:13 +0200

    q

--- lesson6/task12/index.js
+++ lesson6/task12/index.js
@@ -1,13 +1,12 @@
 function cloneArr(arr) {
     let newArray = [];
-
-    //if (Array.isArray(arr)) {
-    // newArray = arr.slice(0);
-    for (let i of arr) {
-        newArray[i] = cloneArr(newArray[i]);
+    if (Array.isArray(arr)) {
+        newArray = arr.slice(0);
+        for (let i of arr) {
+            newArray[i] = cloneArr(newArray[i]);
+        }
+        return newArray;
     }
-    return newArray;
-    //}
 }
 
 /*
