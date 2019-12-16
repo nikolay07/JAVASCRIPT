2b4d0ecde6ba13aaf3f4d4e1610936310f824410
Nik <nikolay0701@ukr.net>
2019-12-16 16:18:50 +0200

    1

--- lesson6/task10/index.js
+++ lesson6/task10/index.js
@@ -1,6 +1,17 @@
 function withdraw(clients, balances, client, amount) {
-
-
+    let money = 0;
+    for (let i of clients) {
+        if (clients[i] === client) {
+            money = i;
+        }
+    };
+    for (let i of balances) {
+        if (balances[i] > amount) {
+            return balances[i] - amount;
+        } else if (balances[i] < amount) {
+            return -1;
+        }
+    }
 }
 
 
@@ -12,6 +23,5 @@ Withdraw
 Считаем что в массиве clients хранятся имена клиентов, а в массиве balances - балансы их счетов соответственно. 
 То есть у клиента по индексу 2 с массива clients, баланс счета - это значение по индексу 2 с массива balances
 Сигнатура - withdraw(clients, balances, client, amount)
-
 На вход гарантировано приходит массив строк, массив чисел, строка (имя клиента) и число (сумма на снятие)
 */
