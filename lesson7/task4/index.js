c3fb9c086d9b02aa91d1fcddd83482d681a26c6c
Nik <nikolay0701@ukr.net>
2019-12-30 22:25:24 +0200

    q

---
+++ lesson7/task4/index.js
@@ -0,0 +1,7 @@
+const getMessagesForBestStudents = (allStudentsNames, failedStudentsNames) =>
+    allStudentsNames
+    .filter(name => !failedStudentsNames.includes(name))
+    .map(name => 'Good job, ' + name);
+const allStudentsNames = ['a', 'b', 'c', 'd'];
+const failedStudentsNames = ['c', 'd'];
+console.log(getMessagesForBestStudents(allStudentsNames, failedStudentsNames));
