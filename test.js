95d7581cf9c5ced30d643c5c55ecb411f9bf57e2
nikolay07 <nikolay.abramovych@gmail.com>
2020-01-29 00:38:25 +0200

    1

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 function getPrimes(n) {
+    let simpleNumber = 0;
     for (let i = 1; i <= n; i++) {
-        if (i % 1 === 0 || i % i === 0)
-            console.log(i)
-    }
+        if (i % 1 === 0 || i % i === 0) {
+            simpleNumber++;
+        };
+        console.log(simpleNumber);
+    };
+
 }
+getPrimes(10)
