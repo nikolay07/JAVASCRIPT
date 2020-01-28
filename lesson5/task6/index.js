9d9aadbfbeaccfb5ff49823a857be30a823b933e
nikolay07 <nikolay.abramovych@gmail.com>
2020-01-29 01:00:42 +0200

    1

--- lesson5/task6/index.js
+++ lesson5/task6/index.js
@@ -1,7 +1,10 @@
-  function getPrimes(number) {
-      for (let i = 1; i <= number; i++) {
-          if (i % 1 === 0 && i % i === 0) {
-              console.log(i);
-          }
-      }
-  }
+function getPrimes(n) {
+    let simpleNumber = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 1 === 0 || i % i === 0) {
+            simpleNumber++;
+        }
+        console.log(simpleNumber);
+    }
+
+}
