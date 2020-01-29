447f0fd7ed629f5d96ac231f405ee840555e356d
nikolay07 <nikolay.abramovych@gmail.com>
2020-01-30 00:26:06 +0200

    1

--- lesson6/task10/index.js
+++ lesson6/task10/index.js
@@ -1,29 +1,16 @@
-  function withdraw(clients, balances, client, amount) {
-      let someMoney = 0;
-      for (let i = 0; i < clients.length; i++) {
-          if (clients[i] === client) {
-              someMoney = i;
-          }
-      };
-      for (let i = 0; i < balances.length; i++) {
-          if (someMoney == i) {
-              if (balances[i] >= amount) {
-                  return balances[i] - amount;
-              } else {
-                  return -1;
-              }
-          }
-      }
-  };
+function withdraw(clients, balances, client, amount) {
+    let newBalance = balances[clients.indexOf(client)] - amount;
+    return newBalance >= 0 ? newBalance : -1
+}
 
+/*
+Withdraw
 
-  /*
-  Withdraw
+Напишите функцию, которая будет снимать деньги со счета определенного клиента и возвращать остаток на счете. 
+Если денег на счете недостаточно метод будет возвращать -1
+Считаем что в массиве clients хранятся имена клиентов, а в массиве balances - балансы их счетов соответственно. 
+То есть у клиента по индексу 2 с массива clients, баланс счета - это значение по индексу 2 с массива balances
+Сигнатура - withdraw(clients, balances, client, amount)
+На вход гарантировано приходит массив строк, массив чисел, строка (имя клиента) и число (сумма на снятие)
 
-  Напишите функцию, которая будет снимать деньги со счета определенного клиента и возвращать остаток на счете. 
-  Если денег на счете недостаточно метод будет возвращать -1
-  Считаем что в массиве clients хранятся имена клиентов, а в массиве balances - балансы их счетов соответственно. 
-  То есть у клиента по индексу 2 с массива clients, баланс счета - это значение по индексу 2 с массива balances
-  Сигнатура - withdraw(clients, balances, client, amount)
-  На вход гарантировано приходит массив строк, массив чисел, строка (имя клиента) и число (сумма на снятие)
-  */
+*/
