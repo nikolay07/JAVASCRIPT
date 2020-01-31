45368c701f637c6ef111de0a5de0dc7251e8de53
nikolay07 <nikolay.abramovych@gmail.com>
2020-01-31 20:26:11 +0200

    1

--- lesson9/hw1/index.js
+++ lesson9/hw1/index.js
@@ -14,30 +14,17 @@ const object = {
 };
 
 function getCustomersList(object) {
-
-    const copyObject = Object.assign({}, object)
-    const valueObject = Object.values(copyObject)
-    const sortedArray = valueObject.sort((a, b) => { return a.age - b.age })
-    return sortedArray;
+    const copyObj = Object.assign({}, object)
+    const objectToArray = Object.entries(copyObj)
+    const addIdValue = objectToArray.map((a) => ({ id: a[0], ...copyObj[a[0]] }))
+    const sortArrayOfObjects = addIdValue.sort((a, b) => a.age - b.age)
+    return sortArrayOfObjects;
 }
 console.log(getCustomersList(object));
 
 /*
 
 Преобразить объект в массив - part 1
-
-Данные о пользвателях храняться в виде объекта в формате
- const customers = {
- 'customer-id-1': {
- name: 'William',
- age: 54
- },
- 'customer-id-2': {
- name: 'Tom',
- age: 17
- },
- ...
- };
 Нужно написать функцию, которая такой объект преобразует в массив и отсортирует
 пользователей по возрасту от самого младшего до старшего
 Основные требования:
@@ -45,6 +32,11 @@ console.log(getCustomersList(object));
 2. Функция должна вернуть список пользователей в виде массива, отсортированного по возрасту
 по возрастанию.
 3. Элементы итогового массива должны включать поле id, значением которого является ключ из
-исходного объекта
+исходного объекта.
+Решаем.
+1.Скопировать объект в новый объект
+2.Перевести массив в объект.
+3.На каждом проходе добавить  id: a[0] из реструктурированого скопированого объекта ...copyObj[a[0]] и заворачиваем его в объект {}.
+4. Отсортировать массив объекта по значению age.
 
 */
