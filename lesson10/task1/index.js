6a5bd42e95508175bc07d4ed70d6383ee31dfed2
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-02 11:56:35 +0200

    1

---
+++ lesson10/task1/index.js
@@ -0,0 +1,54 @@
+numbers = [NaN, 10.1, 11, '17', 7, 'text', -'87', 0, '14.5']
+
+function getFiniteNumbers(numbers) {
+    const result = numbers.slice().filter(number => Number.isFinite(number))
+    return result;
+}
+console.log(getFiniteNumbers(numbers))
+
+function getFiniteNumbersV2(numbers) {
+    const result = numbers.slice().filter((a) => isFinite(a))
+    return result;
+}
+console.log(getFiniteNumbersV2(numbers))
+
+function getNaN(numbers) {
+    const result = numbers.slice().filter((a) => Number.isNaN(a))
+    return result;
+}
+console.log(getNaN(numbers))
+
+function getNaNV2(numbers) {
+    const result = numbers.slice().filter((a) => isNaN(a))
+    return result;
+}
+console.log(getNaNV2(numbers))
+
+function getIntegers(numbers) {
+    const result = numbers.slice().filter((a) => Number.isInteger(a))
+    return result;
+}
+console.log(getIntegers(numbers))
+
+console.log(numbers)
+
+/*
+Проверка чисел
+
+Создайте функции, которые будут искать указанные числа в массиве
+Основные требования:
+1. Функция с названием getFiniteNumbers должна отбросить все элементы исходного массива,
+которые не являются конечными числами. Для проверки должна использовать метод
+Number.isFinite
+2. Функция с названием getFiniteNumbersV2 должна отбросить все элементы исходного массива,
+которые не являются конечными числами. Для проверки должна использовать метод isFinite
+3. Функция с названием getNaN должна вернуть все элементы исходного массива, которые
+имеют значение NaN. Для проверки должна использовать метод Number.isNaN
+4. Функция с названием getNaNV2 должна вернуть все элементы исходного массива, которые
+имеют значение NaN. Для проверки должна использовать метод isNaN
+5. Функция с названием getIntegers должна отбросить все элементы исходного массива, которые
+не являются целыми числами. Для проверки должна использовать метод
+6. Во всех ф-циях исходный массив должен оставаться неизменным
+7. Сравните результаты работы методов Number.isNaN vs isNaN и Number.isFinite vs isFinite
+
+*/
