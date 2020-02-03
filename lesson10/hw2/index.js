4bd07b45a2d0593400d9208eb048bef033c060b2
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-03 14:31:32 +0200

    1

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -5,19 +5,17 @@ function getRandomNumbers(length, min, max) {
     if ((Math.ceil(max) === Math.ceil(min) && (max - min < 1)) || length <= 0 || max < min) {
         return null;
     }
-    let defaultArr = []
-    let randomNum = Math.trunc(Math.random() * (max - min) + min)
-    arr.forEach(a => defaultArr.push(randomNum))
+    let defaultArr = [];
+    for (let i = 0; i < length; i++) {
+        let randomNum = Math.trunc(Math.random() * (max - min) + min);
+        defaultArr[i] = randomNum;
+    }
 
     return defaultArr;
 }
 console.log(getRandomNumbers(10, 1.21, 16.5))
 
 /* 
-
-for (let i = 0; i < length; i++) {
-        defaultArr[i] = Math.trunc(Math.random() * (max - min) + min);
-    }
 Массив случайных чисел
 
 Создайте функцию, которая будет возвращать массив целых случайных чисел из заданного
