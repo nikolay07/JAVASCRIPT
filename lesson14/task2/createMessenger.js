b50ffaef9299ddb5fb46c53739510640d44f61e3
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-06 13:43:06 +0200

    1

---
+++ lesson14/task2/createMessenger.js
@@ -0,0 +1,48 @@
+'use strict'
+
+export default function createMessenger() {
+    let message = 'Just learn it';
+    let sender = 'Gromcode';
+
+    function sendMessage(name) {
+        let text = `${name}, ${message}! Your ${sender}`;
+        console.log(text);
+    }
+
+    function setMessage(newMassage) {
+        message = newMassage.bold();
+    }
+
+    function setSender(textBold) {
+        sender = sender.bold()
+    }
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    }
+}
+
+
+/*
+
+Создайте ф-цию, которая будет создавать независимые счетчики
+
+const masseger1 = createMessenger();
+const masseger2 = createMessenger();
+const masseger3 = createMessenger();
+
+masseger1.sendMessage('Ann');
+masseger2.setMessage('Just learn it');
+masseger3.setMessage('Gromcode');
+
+masseger1.sendMessage('Bob');
+masseger2.setMessage('Just do it');
+masseger3.setMessage('Nike');
+
+masseger1.sendMessage('Tom');
+masseger2.setMessage('Just think it');
+masseger3.setMessage('Dream');
+
+
+*/
