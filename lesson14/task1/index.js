dbb14cdde5184acb63f33a5949085af109ada5ca
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-06 12:07:04 +0200

    1

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -2,12 +2,12 @@ let message = 'Just learn it';
 
 export function sendMessage(name) {
     const sender = 'Gromcode';
-    return console.log(`${name}, ${message}! Your ${sender}`);
+    console.log(`${name}, ${message}! Your ${sender}`);
 }
-sendMessage('Ann');
 
 function setMessage(newMassage) {
     return message = newMassage;
 }
+sendMessage('Ann');
 setMessage('Good job');
 sendMessage('Ann');
