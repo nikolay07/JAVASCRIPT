f9671e6d1c6ee77c773546ec043b13367837e4cb
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-07 16:21:06 +0200

    1

--- lesson16/hw1/index.test.js
+++ lesson16/hw1/index.test.js
@@ -3,3 +3,11 @@ it('should get number', () => {
     const result = createArrayOfFunctions(9)[5]();
     expect(result).toEqual(5);
 });
+it('should get if transmitted not a number', () => {
+    const result = createArrayOfFunctions('');
+    expect(result).toEqual(null);
+});
+it('should get if transmitted nothing', () => {
+    const result = createArrayOfFunctions();
+    expect(result).toEqual([]);
+});
