ecb3d11dd3c81631b2560a27a5fd571391ffcc71
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-08 22:55:54 +0200

    1

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,5 +1,5 @@
-export const bind = (func, context, arguments) => {
-    return function() {
+export const bind = (func, context, arg) => {
+    return function(arg1, arg2) {
         func.call(this, ...arguments);
     }
-}
+};
