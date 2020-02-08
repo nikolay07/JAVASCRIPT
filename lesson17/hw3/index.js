85541cec52b18df1e29a534a4a9074acb8258471
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-08 23:45:33 +0200

    1

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,5 +1,12 @@
 export const bind = (func, context, argn1, arg2) => {
     return function() {
-        func.apply(context, arguments);
+        func.apply(this, arguments);
     }
 };
+const fixCont = {
+    fixedArg() {
+        console.log(`${this}`);
+    }
+}
+const newBind = bind(fixCont.fixedArg);
+newBind.call();
