9fca4f7614158fddd1d0261fe9491ab1a9898cf4
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-09 13:08:03 +0200

    1

--- lesson17/hw2/index.js
+++ lesson17/hw2/index.js
@@ -12,7 +12,7 @@ export const timer = {
         }, 5000);
     },
     getTime() {
-        if (`${this.secondsPassed}` < 10) { return `${this.minsPassed}:${"0" + this.secondsPassed}` }
+        if (`${this.secondsPassed}` < 10) { return `${this.minsPassed}:${"0" + this.secondsPassed}` };
         return (`${this.minsPassed}:${this.secondsPassed}`);
     },
     stopTimer() {
