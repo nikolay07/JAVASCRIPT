48c2a75be8648d6b2639cb9b9771d822a023a184
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-11 01:04:17 +0200

    1

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -2,7 +2,7 @@ let obj = {};
 export function getOwnProps(obj) {
     let propArr = [];
     for (let prop in obj) {
-        if (typeof prop !== 'function' && Object.keys(obj)) {
+        if (typeof prop !== 'function' && obj.hasOwnProperty(prop)) {
             propArr.push(prop);
         }
     }
