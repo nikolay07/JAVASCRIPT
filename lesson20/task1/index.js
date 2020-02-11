43de54c997b8251f7afb021d813da33278803778
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-11 16:51:51 +0200

    Конструктор объектов

--- lesson20/task1/index.js
+++ lesson20/task1/index.js
@@ -24,7 +24,7 @@ const user1 = new User('Bob', 25);
 const user2 = new User('Tom', 19);
 console.log(user1); //User {name: "Bob", age: 25}
 console.log(user2); //User {name: "Tom", age: 19}
-console.log(User.createEmpty()); //User {name: '', age: ''}
+console.log(User.createEmpty()); //User {name: '', age: null}
 
 /*Конструктор объектов
 
