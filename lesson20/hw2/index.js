a1368cc979d5f41cd8636796a91e8c031e0b2cf2
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-12 15:39:54 +0200

    1

--- lesson20/hw2/index.js
+++ lesson20/hw2/index.js
@@ -24,10 +24,10 @@ export class UserRepository {
     }
 
     getUserNames() {
-        return this._name.filter((a) => a.name);
+        return this._name.map((a) => a.name);
     }
     getUserIds() {
-        return this._id.filter((a) => a.id);;
+        return this._id.map((a) => a.id);;
     }
     getUserNameById() {
         return this._sessionId
