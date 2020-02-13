1a94dde9dce5fff391a64cbb0d26a3cca3da2d96
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-13 12:33:01 +0200

    Todo List/ Список дел - добавление, изменение дел - part 1

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -1,4 +1,4 @@
-/*Список дел - добавление, изменение дел - part 1*/
+/*Todo List/ Список дел - добавление, изменение дел - part 1*/
 const tasks = [
     { text: 'Buy milk', done: false },
     { text: 'Pick up Tom from airport', done: false },
