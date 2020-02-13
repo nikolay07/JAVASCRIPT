26dd3d11ecb75af8186ff44aac9dfffda369b3f3
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-13 13:01:05 +0200

    This в callback

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,3 +1,4 @@
+/*This в callback. Написать объект, который может разослать приглашения всем гостям на праздник от 18 лет*/
 export const event = {
     message: 'Welcome to the party!',
     guests: [{ name: 'John', age: 21, email: 'john@server.com', }, { name: 'Ann', age: 16, email: 'ann@server.com', }, { name: 'Merry', age: 19, email: 'merry@server.com', }],
