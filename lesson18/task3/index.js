15358feb96dedbb3d1b0c7494f0cd2e0834cea0c
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-13 12:55:18 +0200

    Работа с псевдомассивами

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,3 +1,4 @@
+/*Работа с псевдомассивами. Написать функцию, принимающую числа как аргументы и возвращает сумму квадратов этих чисел.*/
 export function sumOfSquares() {
     return [...arguments].reduce((acc, el) => { return acc + el * el }, 0);
 }
