1f4d4dd035de5e33089161bda5ca7529fa8e7161
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-13 13:12:35 +0200

    Переадресация вызова

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -1,3 +1,4 @@
+/*Переадресация вызова. Создать функцию, которая будет откладывать вызов на указанное количество времени*/
 export function defer(func, ms) {
     return function() {
         setTimeout(() => func.apply(this, arguments), ms);
