04e0fc0c85ca647c2c96b41585fbd42b3a3f2adc
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-14 13:29:16 +0200

    Всплытие и погружение событий

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -26,3 +26,35 @@ const getListItems = listItems => {
     listElem.append(...listItemElems);
 };
 getListItems(tasks);
+/*
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+const getListItems = listItems => {
+    const listElem = document.querySelector('.list'); //назначаем класс (.list) в переменную listElem;
+    const listItemElems = listItems.sort((a, b) => a.done - b.done) // сортируем массив tasks по ключу объекта done;
+        .map(({ text, done }) => {
+            const listItemElem = document.createElement('li'); //cоздаёт новый элемент(.createElement) с тегом li;
+            listItemElem.classList.add('list__item'); //добавляем новый класс (.classList.add) к li;
+            if (done) {
+                listItemElem.classList.add('list__item_done'); // если значение ключа done=true - добавляем новый класс к этому тегу li;
+            }
+            const checkboxElem = document.createElement('input'); //cоздаёт новый элемент тип input (.createElement) ;
+            checkboxElem.setAttribute('type', 'checkbox'); //  устанавливает атрибут type='checkbox' для input;
+            checkboxElem.checked = done;
+            checkboxElem.classList.add('list__item-checkbox'); //добавляем новый класс к input;
+            listItemElem.append(checkboxElem, text); // вставляем в элемент li: созданныый input(checkboxElem) c текстом (text)
+            console.log(listItemElem);
+            return listItemElem;
+
+        });
+    listElem.append(...listItemElems); //вставляем в переменную listElem деструктурированый listItemElems;
+};
+getListItems(tasks);
+console.log(getListItems(tasks));
+//
+*/
--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -4,6 +4,7 @@ const elemDiv = document.querySelector('.rect_div');
 const elemP = document.querySelector('.rect_p');
 const elemSpan = document.querySelector('.rect_span');
 
+
 const getClick = (text, color) => {
     const eventList = document.querySelector('.events-list');
     eventList.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
@@ -17,7 +18,7 @@ const showGreyDiv = getClick.bind(null, 'DIV', 'grey');
 const showGreyP = getClick.bind(null, 'P', 'grey');
 const showGreySpan = getClick.bind(null, 'SPAN', 'grey');
 
-elemDiv.addEventListener('click', showGreyDiv, true);
+elemDiv.addEventListener('click', showGreyDiv, true); // .addEventListener() регистрирует обработчик события
 elemDiv.addEventListener('click', showGreenDiv);
 
 elemP.addEventListener('click', showGreyP, true);
@@ -25,3 +26,37 @@ elemP.addEventListener('click', showGreenP);
 
 elemSpan.addEventListener('click', showGreySpan, true);
 elemSpan.addEventListener('click', showGreenSpan);
+
+const clearBtn = document.querySelector('.clear-btn');
+const removeBtn = document.querySelector('.remove-handles-btn');
+const attachBtn = document.querySelector('.attach-handles-btn');
+
+const getClickBtn = () => {
+    const eventsListElem = document.querySelector('.events-list');
+    eventsListElem.innerHTML = ''; //очищет содержимое класса .events-list
+};
+const getRemove = () => {
+    elemDiv.removeEventListener('click', showGreyDiv, true); //удаляет обработчик события зарегистрированного ранее с помощью addEventListener()
+    elemDiv.removeEventListener('click', showGreenDiv);
+
+    elemP.removeEventListener('click', showGreyP, true);
+    elemP.removeEventListener('click', showGreenP);
+
+    elemSpan.removeEventListener('click', showGreySpan, true);
+    elemSpan.removeEventListener('click', showGreenSpan);
+
+};
+const getAttach = () => {
+    elemDiv.addEventListener('click', showGreyDiv, true);
+    elemDiv.addEventListener('click', showGreenDiv);
+
+    elemP.addEventListener('click', showGreyP, true);
+    elemP.addEventListener('click', showGreenP);
+
+    elemSpan.addEventListener('click', showGreySpan, true);
+    elemSpan.addEventListener('click', showGreenSpan);
+};
+
+clearBtn.addEventListener('click', getClickBtn);
+removeBtn.addEventListener('click', getRemove);
+attachBtn.addEventListener('click', getAttach);
