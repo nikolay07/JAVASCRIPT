fe32353d870f5f8edb33af225f21daaecc1ed707
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-17 18:26:43 +0200

    1

--- lesson22/hw1/index.js
+++ lesson22/hw1/index.js
@@ -1,47 +1,61 @@
 /*Список дел - добавление, изменение дел - part 1. Добавить возможность создавать дела и отмечать как выполненые / не выполненные*/
+
+//Список задач по умолчанию
 const tasks = [
-    { text: 'Buy milk', done: false, date: new Date },
-    { text: 'Pick up Tom from airport', done: false, date: Date.now() },
-    { text: 'Visit party', done: false, date: new Date },
-    { text: 'Visit doctor', done: false, date: new Date },
-    { text: 'Buy meat', done: false, date: new Date },
+    { text: 'Buy milk', done: false, date: Date.now(), },
+    { text: 'Pick up Tom from airport', done: false, date: Date.now() + 0.1, },
+    { text: 'Visit party', done: false, date: Date.now() + 0.2, },
+    { text: 'Visit doctor', done: true, date: Date.now() + 0.3, },
+    { text: 'Buy meat', done: true, date: Date.now() + 0.4, },
 ];
 
+//Создать новую задачу
+const createNewTask = () => {
+    const task = document.querySelector('.task-input');
+    if (task.value == '') return;
+    const newTask = {
+        text: task.value,
+        done: false,
+        date: Date.now(),
+    }
+    tasks.push(newTask);
+    getListItems(tasks);
+    task.value = '';
+};
+const createTaskButton = document.querySelector('.create-task-btn');
+createTaskButton.addEventListener('click', createNewTask);
+
+//Сортируем, очищаем, обновляем список задач
 const getListItems = listItems => {
     const listElem = document.querySelector('.list');
-    const listItemElems = listItems.sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
+    listElem.innerHTML = '';
+
+    const listItemsElems = listItems
+        .sort((a, b) => b.date - a.date)
+        .sort((a, b) => a.done - b.done)
+        .map(({ text, done, date }) => {
             const listItemElem = document.createElement('li');
             listItemElem.classList.add('list__item');
             if (done) {
                 listItemElem.classList.add('list__item_done');
             }
-            const checkboxElem = document.createElement('input');
-            checkboxElem.setAttribute('type', 'checkbox');
-            checkboxElem.checked = done;
-            checkboxElem.classList.add('list__item-checkbox');
-            listItemElem.append(checkboxElem, text);
+            const cheboxElem = document.createElement('input');
+            cheboxElem.setAttribute('type', 'checkbox');
+            cheboxElem.setAttribute('id', date)
+            cheboxElem.classList.add('list__item-checkbox');
+            cheboxElem.checked = done;
+            cheboxElem.addEventListener('click', getChecked);
+            listItemElem.append(cheboxElem, text);
             return listItemElem;
-
         });
-    listElem.append(...listItemElems);
-};
+    return listElem.append(...listItemsElems);
+}
 
-const createNewElem = () => {
-    const task = document.querySelector('.task-input');
-    const newTask = { text: value, done: false, date: new Date };
-    if (newTask.text == '') { return };
-    tasks.push(newTask);
-    task.value = '';
-};
-const clickToCreate = document.querySelector('.create-task-btn');
-clickToCreate.addEventListener('click', createNewElem);
+function getChecked() {
+    let id = this.id;
+    let idTask = tasks.find(a => a.date == id);
+    idTask.done = this.checked;
+    getListItems(tasks);
+}
 
 getListItems(tasks);
-
-
-//'click' checkbox = done: true
-//Create btn('click', func)
-//func = create new task(text) + add to list. Clear task-input(task-input ='')
-//if (task(text)="") break
-//sort done, sort byDate
