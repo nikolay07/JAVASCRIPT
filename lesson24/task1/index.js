ec1aad304f9002ee656b78d5f4e4be19850b5dac
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-19 22:34:54 +0200

    1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,4 +1,5 @@
-// Установка компонентов даты
+// Установка компонентов даты. Функцию, которая выведет день недели через n дней после указанной даты.
+
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Fr', 'St']
 export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
