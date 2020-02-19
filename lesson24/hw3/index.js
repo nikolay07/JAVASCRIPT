f4231aea6053b8496181ac7810d51ee49063d413
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-19 23:12:10 +0200

    Todo List - Список дел - добавление, изменение дел - part 2.

--- lesson24/hw3/index.js
+++ lesson24/hw3/index.js
@@ -1,4 +1,55 @@
-//Дни рождения студентов. Функция, группирующая студентов по дню рождения.
-const students = [{ name: 'Tom', birthDate: '01/15/2010' }, { name: 'Ben', birthDate: '01/21/2009' }, { name: 'Sam', birthDate: '03/01/2012' }];
-export const studentsBirthDays = (students) => {};
-// {Jan: ['Tom', 'Ben'], Mar: ['Sam']}
+//Todo List - Список дел - добавление, изменение дел - part 2. Список с возможностью создавать дела и отмечать как выполненые / не выполненные.
+const tasks = [
+    { text: 'Buy milk', done: false, date: new Date },
+    { text: 'Pick up Tom from airport', done: false, date: new Date },
+    { text: 'Visit party', done: false, date: new Date },
+    { text: 'Visit doctor', done: true, date: new Date },
+    { text: 'Buy meat', done: true, date: new Date },
+];
+const createNewTask = () => {
+    const task = document.querySelector('.task-input');
+    if (task.value == '') return;
+    const newTask = {
+        text: task.value,
+        done: false,
+        date: new Date,
+    }
+    tasks.push(newTask);
+    getListItems(tasks);
+    task.value = '';
+};
+const createTaskButton = document.querySelector('.create-task-btn');
+createTaskButton.addEventListener('click', createNewTask);
+
+
+const getListItems = listItems => {
+    const listElem = document.querySelector('.list');
+    listElem.innerHTML = '';
+    const listItemElems = listItems
+        .sort((a, b) => b.date - a.date)
+        .sort((a, b) => a.done - b.done)
+        .map(({ text, done, date }) => {
+            const listItemElem = document.createElement('li');
+            listItemElem.classList.add('list__item');
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            const cheboxElem = document.createElement('input');
+            cheboxElem.setAttribute('type', 'checkbox');
+            cheboxElem.classList.add('list__item-checkbox');
+            cheboxElem.checked = done;
+            listItemElem.append(cheboxElem, text);
+            cheboxElem.setAttribute('id', date)
+
+            function getChecked() {
+                const id = this.id;
+                const idByTask = tasks.find(a => a.date == id);
+                idByTask.done = this.checked;
+                getListItems(tasks);
+            }
+            cheboxElem.addEventListener('click', getChecked);
+            return listItemElem;
+        });
+    listElem.append(...listItemElems);
+};
+getListItems(tasks);
