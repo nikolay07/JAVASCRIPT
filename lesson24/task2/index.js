f1309e7da1a5324a26fd4345efd12bc90fbcf25e
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-19 22:55:25 +0200

    1

---
+++ lesson24/task2/index.js
@@ -0,0 +1,9 @@
+//Форматирование даты - part 1. Функция, которая возвращает время в Гринвиче
+const formatter = new Intl.DateTimeFormat('en', {
+    timeZone: 'UTC',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+});
+export const getGreenwichTime = date => formatter.format(date);
+console.log(getGreenwichTime(new Date()));
