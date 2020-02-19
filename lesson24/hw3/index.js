0609a4d59355f3d1074158ac2e84b5d63e555406
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-19 23:09:03 +0200

    Дни рождения студентов

---
+++ lesson24/hw3/index.js
@@ -0,0 +1,4 @@
+//Дни рождения студентов. Функция, группирующая студентов по дню рождения.
+const students = [{ name: 'Tom', birthDate: '01/15/2010' }, { name: 'Ben', birthDate: '01/21/2009' }, { name: 'Sam', birthDate: '03/01/2012' }];
+export const studentsBirthDays = (students) => {};
+// {Jan: ['Tom', 'Ben'], Mar: ['Sam']}
