c3700a76c5da43e3b88ef388c6e95fcb600d46ef
nikolay07 <nikolay.abramovych@gmail.com>
2020-02-19 22:32:10 +0200

    1

---
+++ lesson24/task1/index.js
@@ -0,0 +1,10 @@
+//Установка компонентов даты
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Fr', 'St']
+const dayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+    return weekDays[new Date(dateInFuture).getDay()];
+
+};
+const result = dayOfWeek(new Date(), n);
+console.log(result);
