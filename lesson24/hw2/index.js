6b31d660ce8dcb288b25f4e6665f4fc8a1ce838c
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-04 21:18:31 +0200

    1

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -1,15 +1,22 @@
 //Дни рождения студентов. Функция, группирующая студентов по дню рождения.
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
-    'Oct', 'Nov', 'Dec'
-];
+// const students = [
+//     { name: 'Tom', birthDate: '01/15/2010' },
+//     { name: 'Ann', birthDate: '03/19/2011' },
+//     { name: 'Ben', birthDate: '01/21/2009' },
+//     { name: 'Sam', birthDate: '03/01/2012' }
+// ];
 
 export const studentsBirthDays = students => {
-
     let birthDays = {};
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+        'Oct', 'Nov', 'Dec'
+    ];
     students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
         .map(elem => {
+
             if (birthDays[months[new Date(elem.birthDate).getMonth()]] === undefined) {
                 birthDays[months[new Date(elem.birthDate).getMonth()]] = [];
+
             }
             birthDays[months[new Date(elem.birthDate).getMonth()]].push(elem.name);
         });
