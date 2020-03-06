83091dece6cc2e2c0500bfbeeb60851fb46f6f95
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 13:46:52 +0200

    1

--- lesson21/hw3/index.js
+++ lesson21/hw3/index.js
@@ -1,25 +1,23 @@
-export default () => {
-    function getTitle() {
-        console.log(document.querySelector('.title').textContent);
-        return document.querySelector('.title').textContent;
-    };
-    getTitle();
-
-    function getDescription() {
-        console.log(document.querySelector('.about').innerText);
-        return document.querySelector('.about').innerText;
-    };
-    getDescription();
-
-    function getPlans() {
-        console.log(document.querySelector('.plans').innerHTML);
-        return document.querySelector('.plans').innerHTML;
-    }
-    getPlans();
-
-    function getGoal() {
-        console.log(document.querySelector('.goal').outerHTML);
-        return document.querySelector('.goal').outerHTML;
-    };
-    getGoal();
+export function getTitle() {
+    //console.log(document.querySelector('.title').textContent);
+    return document.querySelector('.title').textContent;
 };
+getTitle();
+
+export function getDescription() {
+    //console.log(document.querySelector('.about').innerText);
+    return document.querySelector('.about').innerText;
+};
+getDescription();
+
+export function getPlans() {
+    // console.log(document.querySelector('.plans').innerHTML);
+    return document.querySelector('.plans').innerHTML;
+}
+getPlans();
+
+export function getGoal() {
+    //console.log(document.querySelector('.goal').outerHTML);
+    return document.querySelector('.goal').outerHTML;
+};
+getGoal();
