a1cf9eb17bad18bc9452fde3a40df202869f6df3
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 13:29:28 +0200

    1

--- lesson21/hw2/index.js
+++ lesson21/hw2/index.js
@@ -1,13 +1,15 @@
-export default function getItemsList() {
-    let elementsList = document.querySelectorAll('.technology');
-    console.dir(elementsList);
-    return elementsList
-}
-getItemsList();
+export default () => {
+    function getItemsList() {
+        let elementsList = document.querySelectorAll('.technology');
+        console.dir(elementsList);
+        return elementsList
+    }
+    getItemsList();
 
-export default function getItemsArray() {
-    let elementsArray = document.querySelectorAll('.tool');
-    console.dir(elementsArray);
-    return Array.from(elementsArray);
-}
-getItemsArray();
+    function getItemsArray() {
+        let elementsArray = document.querySelectorAll('.tool');
+        console.dir(elementsArray);
+        return Array.from(elementsArray);
+    }
+    getItemsArray();
+};
