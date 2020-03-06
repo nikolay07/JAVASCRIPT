84d0de642f68c9dc9e5d95a9d057bfbd79bbfe3e
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 13:41:15 +0200

    1

--- lesson21/hw1/index.js
+++ lesson21/hw1/index.js
@@ -1,15 +1,13 @@
-export default () => {
-    const getTitleElement = () => {
-        let titleElem = document.querySelector('.title');
-        console.dir(titleElem);
-        return titleElem;
-    }
-    getTitleElement();
+export const getTitleElement = () => {
+    let titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+}
+getTitleElement();
 
-    const getInputElement = () => {
-        let inputElem = document.querySelector('input[type = "text"]');
-        console.dir(inputElem);
-        return inputElem;
-    };
-    getInputElement();
+export const getInputElement = () => {
+    let inputElem = document.querySelector('input[type = "text"]');
+    console.dir(inputElem);
+    return inputElem;
 };
+getInputElement();
--- lesson21/hw3/index.js
+++ lesson21/hw3/index.js
@@ -1,23 +1,25 @@
-export default function getTitle() {
-    console.log(document.querySelector('.title').textContent);
-    return document.querySelector('.title').textContent;
-};
-getTitle();
+export default () => {
+    function getTitle() {
+        console.log(document.querySelector('.title').textContent);
+        return document.querySelector('.title').textContent;
+    };
+    getTitle();
 
-export default function getDescription() {
-    console.log(document.querySelector('.about').innerText);
-    return document.querySelector('.about').innerText;
-};
-getDescription();
+    function getDescription() {
+        console.log(document.querySelector('.about').innerText);
+        return document.querySelector('.about').innerText;
+    };
+    getDescription();
 
-export default function getPlans() {
-    console.log(document.querySelector('.plans').innerHTML);
-    return document.querySelector('.plans').innerHTML;
-}
-getPlans();
+    function getPlans() {
+        console.log(document.querySelector('.plans').innerHTML);
+        return document.querySelector('.plans').innerHTML;
+    }
+    getPlans();
 
-export default function getGoal() {
-    console.log(document.querySelector('.goal').outerHTML);
-    return document.querySelector('.goal').outerHTML;
+    function getGoal() {
+        console.log(document.querySelector('.goal').outerHTML);
+        return document.querySelector('.goal').outerHTML;
+    };
+    getGoal();
 };
-getGoal();
