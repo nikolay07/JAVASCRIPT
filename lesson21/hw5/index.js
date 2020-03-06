fc8135c9d7a52e84f5abc5141fcc6f28c420449c
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 16:28:40 +0200

    1

--- lesson21/hw5/index.js
+++ lesson21/hw5/index.js
@@ -1,4 +1,5 @@
+const buttonText = '<button>button text</button>';
 export function setButton(buttonText) {
-    document.querySelector('body').innerHTML = buttonText;
+    return document.querySelector('body').innerHTML = buttonText;
     //return document.querySelector('body').textContent = buttonText;
 };
