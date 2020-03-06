21246f4ac21672dc7636a74f29965ba2543b2c7c
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 16:25:50 +0200

    1

--- lesson21/hw5/index.js
+++ lesson21/hw5/index.js
@@ -1,4 +1,4 @@
 export function setButton(buttonText) {
-    return document.querySelector('body').innerHTML = buttonText;
+    document.querySelector('body').innerHTML = buttonText;
     //return document.querySelector('body').textContent = buttonText;
 };
