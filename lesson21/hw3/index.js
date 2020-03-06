d221fd9525b87b5e38588bfc1c89236ae5c16e52
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 16:20:06 +0200

    1

--- lesson21/hw3/index.js
+++ lesson21/hw3/index.js
@@ -1,19 +1,18 @@
 export function getTitle() {
     return document.querySelector('.title').textContent;
 };
-getTitle();
+
 
 export function getDescription() {
     return document.querySelector('.about').innerText;
 };
-getDescription();
+
 
 export function getPlans() {
     return document.querySelector('.plans').innerHTML;
 }
-getPlans();
+
 
 export function getGoal() {
     return document.querySelector('.goal').outerHTML;
 };
-getGoal();
