06dfeb721d437ca40e86be4ae2fa68bf77ccff5d
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-06 13:21:34 +0200

    1

--- lesson21/hw1/index.js
+++ lesson21/hw1/index.js
@@ -1,13 +1,15 @@
-export default function getTitleElement() {
-    let titleElem = document.querySelector('.title');
-    console.dir(titleElem);
-    return titleElem;
-}
-getTitleElement();
+export default () => {
+    function getTitleElement() {
+        let titleElem = document.querySelector('.title');
+        console.dir(titleElem);
+        return titleElem;
+    }
+    getTitleElement();
 
-export default function getInputElement() {
-    let inputElem = document.querySelector('input[type = "text"]');
-    console.dir(inputElem);
-    return inputElem;
+    export function getInputElement() {
+        let inputElem = document.querySelector('input[type = "text"]');
+        console.dir(inputElem);
+        return inputElem;
+    };
+    getInputElement();
 };
-getInputElement();
