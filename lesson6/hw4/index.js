161eb2638dc8534c4af14664447fa347bd021799
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-10 00:46:20 +0200

    1

--- lesson6/hw4/index.js
+++ lesson6/hw4/index.js
@@ -1,4 +1,4 @@
-const increaseEvenEi = (arr, delta) => {
+const increaseEvenEl = (arr, delta) => {
     if (!Array.isArray) return null;
     return arr.map(elem => elem % 2 === 0 ? elem + delta : elem);
 };
