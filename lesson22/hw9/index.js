31e9401165ecc774bcc43727094b978c9137f806
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-14 20:42:36 +0200

    1

--- lesson22/hw9/index.js
+++ lesson22/hw9/index.js
@@ -1,6 +1,10 @@
 let btns = document.querySelectorAll('.pagination__page');
 let page = document.querySelector('.pagination');
 const handleClick = (event) => {
-    btns.forEach(elem => console.log(elem));
+    // btns.forEach(elem => {
+    //     console.log(elem.dataset.pageNumber)
+    //     event.stopPropagation();
+    // });
+    console.log(event.target.dataset.pageNumber);
 }
 page.addEventListener('click', handleClick);
---
+++ lesson8/hw8/Index.js
@@ -0,0 +1,21 @@
+let keysList = ['name', 'address', 'age'];
+let valuesList = ['Bob', 'Ukraine', 34];
+buildObject = (keysList, valuesList) => {
+    let obj = keysList.reduce((acum, elem, index) => ({
+        ...acum,
+        [elem]: index
+    }), {})
+
+    // let obj = {};
+    // for (let i = 0; i < keysList.length; i++) {
+    //     for (let j = 0; j < valuesList.length; j++) {
+    //         if (i === j) {
+    //             obj[keysList[i]] = valuesList[j];
+    //         }
+    //     }
+    // }
+    return obj;
+}
+
+buildObject(keysList, valuesList);
+console.log(buildObject(keysList, valuesList));
