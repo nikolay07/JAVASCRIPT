c3e44f6087bd330058ba9442ea568c674ec59e48
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-14 23:25:22 +0200

    1

--- lesson22/hw9/index.js
+++ lesson22/hw9/index.js
@@ -1,5 +1,6 @@
 const page = document.querySelector('.pagination');
-const handleClick = (event) => {
+
+function handleClick(event) {
     const paginationPage = event.target.closest('.pagination__page');
     console.log(paginationPage.dataset.pageNumber);
 }
