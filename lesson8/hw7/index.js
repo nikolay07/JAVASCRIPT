674b41ced5c16b085c15bf64ca47943d93a852ac
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-14 22:37:39 +0200

    1

--- lesson8/hw7/index.js
+++ lesson8/hw7/index.js
@@ -1,12 +1,14 @@
 // let object1 = { name: 'Nik', age: 34 };
-// let object2 = { name: 'Nik', age: 34 };
+// let object2 = { name: 'Nik', age: 33 };
 // let object3 = { name: 'Nik', age: 35 };
 
 const compareObjects = (obj1, obj2) => {
-    let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
-    let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
-    return elem1.every((elem, index) => elem === elem2[index]);
+    // let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
+    // let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
+    // return elem1.every((elem, index) => elem === elem2[index]);
+
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
 
 }
 
-// console.log(compareObjects(object1, object2));
+//console.log(compareObjects(object1, object2));
