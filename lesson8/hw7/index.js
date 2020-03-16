98e112ee0e5137c1233c1ed8af901a50626fabf6
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-16 14:46:35 +0200

    1

--- lesson8/hw7/index.js
+++ lesson8/hw7/index.js
@@ -1,30 +1,30 @@
-let object1 = { name: 'Nik', age: 34, };
-let object2 = { age: 34, name: 'Nik' };
-let object3 = { name: 'Nik', age: 35 };
+// let object1 = { name: 'Nik', age: 34, };
+// let object2 = { age: 34, name: 'Nik', };
+// let object3 = { name: 'Nik', age: 35 };
 
 const compareObjects = (obj1, obj2) => {
-    //let someNum;
-    // if (Object.keys(obj1).length === Object.keys(obj2).length) {
-    //     for (let key in obj1) {
-    //         for (let k in obj2) {
-    //             if (key === k && obj1[key] !== obj2[k]) {
-    //                 return false;
-    //             } else { someNum = true }
-    //         }
-    //     }
-    // } else { someNum = false }
-    let elem1 = [].concat(Object.entries(obj1));
-    let elem2 = Object.entries(obj2);
-    console.log(elem1);
-
-
-    // let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
-    // let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
-    // return elem1.every((elem, index) => elem === elem2[index]);
-
+    let arr1 = Object.keys(obj1);
+    let arr2 = Object.keys(obj2)
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i of arr1) {
+        if (obj1[i] !== obj2[i]) {
+            return false;
+        }
+    }
+    return true;
     // JSON.stringify(obj1) === JSON.stringify(obj2);
-    //return someNum;
-
 }
 
-console.log(compareObjects(object1, object2));
+// console.log(compareObjects(object1, object2));
+
+
+
+
+
+
+
+// let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
+// let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
+// return elem1.every((elem, index) => elem === elem2[index]);
