70c6f26f04e34d9e29683445edd587d7aace1b3c
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-16 13:03:36 +0200

    1

--- lesson22/hw9/index.js
+++ lesson22/hw9/index.js
@@ -1,7 +1,23 @@
 const page = document.querySelector('.pagination');
+const pageItem = document.querySelectorAll('.pagination__page');
 
 function handleClick(event) {
-    const paginationPage = event.target.closest('.pagination__page');
-    console.log(paginationPage.dataset.pageNumber);
+    // const paginationPage = event.target.closest('.pagination__page');
+    // console.log(paginationPage.dataset.pageNumber);
+
+    // for (let i = 0; i < pageItem.length; i++) {
+    //     if (event.target === pageItem[i]) {
+    //         console.log(pageItem[i].dataset.pageNumber);
+    //     }
+    // }
+
+    // console.log(event.target.dataset.pageNumber);
+
+    pageItem.forEach(elem => {
+        if (event.target === elem) {
+            console.log(elem.dataset.pageNumber);
+        }
+    })
+
 }
 page.addEventListener('click', handleClick);
--- lesson8/hw7/index.js
+++ lesson8/hw7/index.js
@@ -1,14 +1,30 @@
-// let object1 = { name: 'Nik', age: 34 };
-// let object2 = { name: 'Nik', age: 33 };
-// let object3 = { name: 'Nik', age: 35 };
+let object1 = { name: 'Nik', age: 34, };
+let object2 = { age: 34, name: 'Nik' };
+let object3 = { name: 'Nik', age: 35 };
 
 const compareObjects = (obj1, obj2) => {
+    //let someNum;
+    // if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    //     for (let key in obj1) {
+    //         for (let k in obj2) {
+    //             if (key === k && obj1[key] !== obj2[k]) {
+    //                 return false;
+    //             } else { someNum = true }
+    //         }
+    //     }
+    // } else { someNum = false }
+    let elem1 = [].concat(Object.entries(obj1));
+    let elem2 = Object.entries(obj2);
+    console.log(elem1);
+
+
     // let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
     // let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
     // return elem1.every((elem, index) => elem === elem2[index]);
 
-    return JSON.stringify(obj1) === JSON.stringify(obj2);
+    // JSON.stringify(obj1) === JSON.stringify(obj2);
+    //return someNum;
 
 }
 
-//console.log(compareObjects(object1, object2));
+console.log(compareObjects(object1, object2));
