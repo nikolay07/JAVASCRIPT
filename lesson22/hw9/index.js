b184ea81e2c8defd49c517e465cd69cff56634a4
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-16 14:40:08 +0200

    1

--- lesson22/hw9/index.js
+++ lesson22/hw9/index.js
@@ -1,11 +1,10 @@
 const page = document.querySelector('.pagination');
 const pageItem = document.querySelectorAll('.pagination__page');
 
+
 function handleClick(event) {
     // const paginationPage = event.target.closest('.pagination__page');
     // console.log(paginationPage.dataset.pageNumber);
-
-    // console.log(event.target.dataset.pageNumber);
 
     // for (let i = 0; i < pageItem.length; i++) {
     //     if (event.target === pageItem[i]) {
@@ -13,10 +12,15 @@ function handleClick(event) {
     //     }
     // }
 
-    pageItem.forEach(elem => {
-        if (elem === event.target) {
-            console.log(elem.dataset.pageNumber);
-        }
-    });
+    // pageItem.forEach(elem => {
+    //     if (elem === event.target) {
+    //         console.log(elem.dataset.pageNumber);
+    //     }
+    // });
+
+    console.log(event.target.dataset.pageNumber);
 }
-page.addEventListener('click', handleClick);
+// page.addEventListener('click', handleClick);
+pageItem.forEach(elem => {
+    elem.addEventListener('click', handleClick);
+});
