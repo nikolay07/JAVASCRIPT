c847aeb0c5b55bc2ec87e7fdddacb176353e10c9
nikolay07 <nikolay.abramovych@gmail.com>
2020-03-18 21:57:12 +0200

    1

--- lesson8/hw7/index.js
+++ lesson8/hw7/index.js
@@ -1,6 +1,5 @@
-// let object1 = { name: 'Nik', age: 34, };
-// let object2 = { age: 34, name: 'Nik', };
-// let object3 = { name: 'Nik', age: 35 };
+let object1 = { name: 'Nik', age: 34, };
+let object2 = { age: 34, name: 'Nik', };
 
 const compareObjects = (obj1, obj2) => {
     let arr1 = Object.keys(obj1);
@@ -9,22 +8,9 @@ const compareObjects = (obj1, obj2) => {
         return false;
     }
     for (let i of arr1) {
-        if (obj1[i] !== obj2[i]) {
-            return false;
-        }
+        return (obj1[i] !== obj2[i]) ? false : true;
     }
-    return true;
     // JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
-// console.log(compareObjects(object1, object2));
-
-
-
-
-
-
-
-// let elem1 = Object.entries(obj1).reduce((acum, elem) => acum.concat(elem));
-// let elem2 = Object.entries(obj2).reduce((acum, elem) => acum.concat(elem));
-// return elem1.every((elem, index) => elem === elem2[index]);
+console.log(compareObjects(object1, object2));
--- lesson8/hw8/index.js
+++ lesson8/hw8/index.js
@@ -1,21 +1,50 @@
-// let keysList = ['name', 'address', 'age'];
-// let valuesList = ['Bob', 'Ukraine', 34];
+let keysList = ['name', 'address', 'age'];
+let valuesList = ['Bob', 'Ukraine', 34];
 const buildObject = (keysList, valuesList) => {
-    // let obj = keysList.reduce((acum, elem, index) => ({
-    //     ...acum,
-    //     [elem]: index
-    // }), {})
+    // let obj = {};
+    // for (let i = 0; i < keysList.length; i++) {
+    //     for (let j = 0; j < valuesList.length; j++) {
+    //         if (i === j) {
+    //             obj[keysList[i]] = valuesList[j];
+    //         }
+    //     }
+    // }
+    // return obj;
 
-    let obj = {};
-    for (let i = 0; i < keysList.length; i++) {
-        for (let j = 0; j < valuesList.length; j++) {
-            if (i === j) {
-                obj[keysList[i]] = valuesList[j];
-            }
-        }
-    }
+    // let obj = {}
+    // keysList.map((elem, index) => obj[elem] = valuesList[index])
+    // return obj;
+
+    // let obj = Object.assign(...keysList.map((elem, index) => ({
+    //     [elem]: valuesList[index]
+    // })));
+    // return obj;
+
+    // let obj = Object.assign.apply({}, keysList.map((elem, index) => ({
+    //     [elem]: valuesList[index]
+    // })));
+    // return obj;
+
+    // let obj = keysList.reduce((acum, elem, index) => {
+    //     acum[elem] = valuesList[index];
+    //     return acum;
+    // }, {})
+    // return obj;
+
+    let obj = keysList.reduce((acum, elem, index) => ({...acum, [elem]: valuesList[index] }), {})
     return obj;
+
 }
 
-// buildObject(keysList, valuesList);
-// console.log(buildObject(keysList, valuesList));
+buildObject(keysList, valuesList);
+console.log(buildObject(keysList, valuesList));
+
+
+
+
+// let obj = {
+//     [keysList.shift()]: valuesList.shift(),
+//     [keysList.shift()]: valuesList.shift(),
+//     [keysList.shift()]: valuesList.shift(),
+// }
+// return obj;
