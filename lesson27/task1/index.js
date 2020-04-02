78ea188a2cc7c92002003a533a41fe54247d8df5
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-02 22:53:03 +0300

    1

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,3 +1,15 @@
 const getLocalStorageData = () => {
-
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (e) {
+                newValue = value;
+            }
+            return {
+                ...acc,
+                [key]: newValue
+            }
+        }, {});
 }
