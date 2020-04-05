da2d1d7be7f3cecda1e3b4980c6c1e37af0bc015
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-05 20:23:48 +0300

    1

---
+++ lesson17/hw4/index.js
@@ -0,0 +1,36 @@
+export function calculator(a, b) {
+    switch (this.operation) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '/':
+            return a / b;
+        case '*':
+            return a * b;
+        default:
+            return NaN;
+    }
+}
+
+
+/*
+ * Ф-ция multiplier должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из произведение
+ */
+// ...your code here
+
+/*
+ * Ф-ция summator должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из сумму
+ */
+// ...your code here
+
+/*
+ * Ф-ция twice должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 1 число и возвращать это число умноженное на 2
+ */
+// ...your code here
