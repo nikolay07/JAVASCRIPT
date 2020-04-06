fb9c8d02468b7df11ddddde8f1737f8775ea5e6f
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-06 14:41:23 +0300

    1

--- lesson28/task4/index.js
+++ lesson28/task4/index.js
@@ -1,3 +1,11 @@
-compose(func1, func2) => {
+const add2 = value => value + 2;
+const square = value => value * value;
+const half = value => value / 2;
 
-}
+
+const compose = (...funcs) => value => {
+    return funcs((acc, func) => func(acc), value);
+};
+const superFunc = compose(add2, square, half);
+
+export { compose };
