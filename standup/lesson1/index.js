25ddfc48ea095a2101f87039aaf9007b4f0e8ad8
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-06 13:00:22 +0300

    1

--- standup/lesson1/index.js
+++ standup/lesson1/index.js
@@ -4,14 +4,21 @@ const dayTransactions = [
     { currency: 'EUR', amount: 90, operation: 'sell' },
 ];
 
-const getTotalBalance = transactionsList => {
-    const transactions = transactionsList.reduce((acc, curr) => {
-        const { currency, operation, amount } = curr;
-        acc[currency] = acc[currency] || 0;
-        operation === 'buy' ? acc[currency] += amount : acc[currency] -= amount
-        return acc;
-    }, {});
-    return transactions;
-};
-const result = getTotalBalance(dayTransactions);
-console.log(getTotalBalance(dayTransactions));
+// const getTotalBalance = transactionsList => {
+//     const transactions = transactionsList.reduce((acc, curr) => {
+//         const { currency, operation, amount } = curr;
+//         acc[currency] = acc[currency] || 0;
+//         operation === 'buy' ? acc[currency] += amount : acc[currency] -= amount
+//         return acc;
+//     }, {});
+//     return transactions;
+// };
+// const result = getTotalBalance(dayTransactions);
+// console.log(getTotalBalance(dayTransactions));
+
+
+const con = (obj) => obj.reduce((acc, curr) => {
+    const { currency, operation, amount } = curr;
+    return {...acc, [currency]: (acc[currency] || 0) + (amount * (operation === 'buy' ? -1 : 1)) }
+}, {})
+console.log(con(dayTransactions));
