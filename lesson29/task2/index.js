aa35dfc6e79ac7a9a7f32a6480483504beea7235
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-07 23:42:15 +0300

    1

---
+++ lesson29/task2/index.js
@@ -0,0 +1,16 @@
+export const pinger = (num, period) => {
+    let i = num;
+    console.log('Ping')
+    const interval = setInterval(() => {
+        if (--i > 0) {
+            console.log('Ping');
+        } else {
+            clearInterval(interval);
+        }
+    }, period);
+    // setTimeout(() => {
+
+    // }, num * period);
+};
+
+//pinger(5, 1000);
