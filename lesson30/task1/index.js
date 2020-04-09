9f11c2b19e7eb4eaafe2eabc7b3c806ecbc385be
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-09 19:30:46 +0300

    1

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,31 +1,23 @@
-const addImageWithPromise = imgSrc => {
-    const p = new Promise((resolveImg, resectImg) => {
+export const addImage = imgSrc => {
+    const p = new Promise((resolveCb, rejectCb) => {
         const imgElem = document.createElement('img');
-        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.setAttribute('alt', 'My photo');
         imgElem.src = imgSrc;
         const containerElem = document.querySelector('.page');
         containerElem.append(imgElem);
         const onImageLoaded = () => {
             const { width, height } = imgElem;
-            resolveImg({ width, height });
-        };
+            resolveCb({ width, height });
+        }
         imgElem.addEventListener('load', onImageLoaded);
-        imgElem.addEventListener('error', () => resectImg(new Error('Image load failed')));
+        imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')));
     });
+
     return p;
-}
+};
 const imgSrc = 'https://caricatura.ru/black/korsun/pic/karikatura-vopros_(sergey-korsun)_1597.gif';
-
-const result = addImageWithPromise(imgSrc);
+addImage(imgSrc)
+const result = addImage(imgSrc);
 console.log(result);
-
-// const callback = (error, data) => {
-//         if (error) {
-//             console.log(error);
-//             return;
-//         };
-//         const { width, height } = data;
-//         const sizeElem = document.querySelector('.image-size');
-//         sizeElem.textContent = `${width} x ${height}`;
-//     }
-//addImage(imgSrc, callback);
+result.catch(error => console.log(error));
+result.then(data => console.log(data));
