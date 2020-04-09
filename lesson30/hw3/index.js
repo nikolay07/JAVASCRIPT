7ef81e694d7685570d42d0f624631b251ee139ea
nikolay07 <nikolay.abramovych@gmail.com>
2020-04-09 19:44:33 +0300

    1

---
+++ lesson30/hw3/index.js
@@ -0,0 +1,16 @@
+/*
+ * failedPromise должен зареджектить ошибку new Error('Oops, error!');
+ * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise
+ */
+
+const failedPromise = new Promise((resolve, reject) => {
+    /* ...code here */
+});
+
+/*
+ * выведите в консоль ошибку в ф-ции onError
+ */
+
+failedPromise.catch(function onError(error) {
+    /* ...code here */
+});
