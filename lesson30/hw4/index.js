f93919556eb9934cce53e176378ec28426105750
nikolay07 <nikolay0701@ukr.net>
2020-04-13 01:48:07 +0300

    1

--- lesson30/hw4/index.js
+++ lesson30/hw4/index.js
@@ -1,24 +1,24 @@
+const imgSrc = 'https://ru.wikipedia.org/wiki/PNG#/media/%D0%A4%D0%B0%D0%B9%D0%BB:PNG_transparency_demonstration_1.png';
 const addImageV2 = myImage => {
-        const addImage = new Promise((resolve, reject) => {
-            const img = document.createElement('img');
-            img.setAttribute('alt', 'User avatar');
-            img.src = myImage;
+    const addImage = new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        img.setAttribute('alt', 'User avatar');
+        img.src = myImage;
 
-            const pageElem = document.querySelector('.page');
-            pageElem.append(img);
+        const pageElem = document.querySelector('.page');
+        pageElem.append(img);
 
-            const onImageLoaded = () => {
-                const { width, height } = img;
-                resolve({ width, height });
-            }
+        const onImageLoaded = () => {
+            const { width, height } = img;
+            resolve({ width, height });
+        }
 
-            img.addEventListener('load', onImageLoaded);
-            img.addEventListener('error', () => reject(new Error('Image load failed')));
-        });
-        return addImage;
-    }
-    //const imgSrc = 'https://ru.wikipedia.org/wiki/PNG#/media/%D0%A4%D0%B0%D0%B9%D0%BB:PNG_transparency_demonstration_1.png';
-addImageV2(myImage)
+        img.addEventListener('load', onImageLoaded);
+        img.addEventListener('error', () => reject(new Error('Image load failed')));
+    });
+    return addImage;
+}
+addImageV2(imgSrc)
     .then(data => console.log(data))
     .catch(error => console.log(error));
 
