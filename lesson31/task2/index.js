6a34806984dceb935d9e36ee44af83dea3da6943
nikolay07 <nikolay0701@ukr.net>
2020-04-13 14:58:21 +0300

    1

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -2,14 +2,14 @@ export const anyncCalculator = num => new Promise((resolve) => {
         setTimeout(() => {
             console.log(`Initial value:${num}`);
             resolve(num);
-        }, 3000);
+        }, 500);
     })
     .then(value => new Promise((resolve) => {
         setTimeout(() => {
             const result = value * value;
             console.log(`Squared value:${result}`);
             resolve(result);
-        }, 3000);
+        }, 500);
     }))
     .then(value => {
         const result = value * 2;
