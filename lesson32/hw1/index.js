bc5f4279e8013ab228211ce7a29cd156b5565279
nikolay07 <nikolay0701@ukr.net>
2020-04-13 18:23:31 +0300

    1

---
+++ lesson32/hw1/index.js
@@ -0,0 +1,15 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+/* ...code here */
+export const makePromise = (num) => {
+        return new Promise(resolve => resolve(num))
+    }
+    /*
+     * пример использования
+     */
+makePromise(17)
+    .then(number => {
+        console.log(number); // 17
+    });
---
+++ lesson32/hw2/index.js
@@ -0,0 +1,19 @@
+const promiseNumber1 = Promise.resolve(67);
+const promiseNumber2 = Promise.resolve(23);
+const promiseNumber3 = Promise.resolve(8);
+
+/*
+ * создайте промис и присвойте переменной resultPromise
+ * чтобы в консоль вывелась сумма всех чисел из трех промисов
+ */
+export const resultPromise = /* ...code here */
+
+    resultPromise
+    .then(numbersList => {
+        console.log(numbersList);
+        const sum = numbersList.reduce((acc, num) => acc + num, 0);
+        return sum;
+    })
+    .then(result => {
+        console.log(result); // 98
+    });
