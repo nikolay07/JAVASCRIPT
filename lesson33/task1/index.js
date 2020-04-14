f473ee7b7647f44a52d225c5859bbcc74a0ab2cd
nikolay07 <nikolay0701@ukr.net>
2020-04-15 01:08:06 +0300

    1

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -6,10 +6,25 @@ const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
 
 //https://api.github.com/users/nikola07
+function fetchUserData(userName) {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json())
+}
+const renderUserData = userData => {
+    const { avatar_url, name, location } = userData;
+    userAvatarElem.src = avatar_url;
+    userNameElem.textContent = name;
+    userLocationElem.textContent = location ?
+        `from ${location}` :
+        '';
+
+}
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 const onSearchUser = () => {
-
+    const userName = userNameInputElem.value;
+    fetchUserData(userName)
+        .then(userData => renderUserData(userData))
 };
 showUserBtnElem.addEventListener('click', onSearchUser);
