d1789d3b36ac5f59c3fc220ae65134b148031e22
nikolay07 <nikolay0701@ukr.net>
2020-04-15 00:18:59 +0300

    1

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -1,1 +1,15 @@
 const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+
+userAvatarElem.src = defaultAvatar;
+
+//https://api.github.com/users/nikola07
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+const onSearchUser = () => {
+
+};
+showUserBtnElem.addEventListener('click', onSearchUser);
