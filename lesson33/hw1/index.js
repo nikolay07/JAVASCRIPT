1fea87fd210f6e25f303e8a8dac5d7f11dda518b
nikolay07 <nikolay0701@ukr.net>
2020-04-15 01:41:09 +0300

    1

--- lesson33/hw1/index.js
+++ lesson33/hw1/index.js
@@ -1,8 +1,9 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks'
     /* getTaskById code here */
-const getTasksList = (baseUrl) => {
+const getTasksList = () => {
         return fetch(baseUrl)
-            .then(response => response.json())
+            .then(response =>
+                response.json());
     }
     //getTasksList(baseUrl).then(result => console.log(result))
     /* getTaskById code here */
