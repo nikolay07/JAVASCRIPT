c874aa41a0f16a453d9050e83153c18d821ffbd4
nikolay07 <nikolay0701@ukr.net>
2020-04-15 22:21:32 +0300

    1

--- lesson32/hw1/index.js
+++ lesson32/hw1/index.js
@@ -13,3 +13,18 @@ makePromise(17)
     .then(number => {
         console.log(number); // 17
     });
+
+
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+/* ...code here */
+const makePromise = num => new Promise(resolve => resolve(num));
+/*
+ * пример использования
+ */
+makePromise(17)
+    .then(number => {
+        console.log(number); // 17
+    });
