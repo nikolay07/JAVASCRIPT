986e23fb9f4986a7eaec0c88ef7a7b2082f6c6ae
nikolay07 <nikolay0701@ukr.net>
2020-04-16 22:08:49 +0300

    1

--- lesson34/hw2/index.js
+++ lesson34/hw2/index.js
@@ -1,39 +1,34 @@
-const baseUrl = 'https://5e988dc45eabe7001681c309.mockapi.io/users';
-const headersJson = { 'Content-Type': 'application/json; charset=utf-8' };
 const loginForm = document.querySelector('.login-form');
-const formInputs = document.querySelectorAll('.form-input');
-const formSubmitBtn = document.querySelector('.submit-button');
-const errorTextElem = document.querySelector('.error-text');
+const submitBtn = document.querySelector('.submit-button');
+const errorText = document.querySelector('.error-text');
+const allUsers = 'https://5e6b6f91d708a000160b48ba.mockapi.io/api/v1/use';
+const headersJson = { 'Content-Type': 'application/json; charset=utf-8' };
 
-const onInputChange = () => {
+const loginForm = document.querySelector('.login-form')
+const formInputs = document.querySelectorAll('.form-input')
+const formSubmitBtn = document.querySelector('.submit-button')
+const errorTextElem = document.querySelector('.error-text')
+
+const onInputChange = (e) => {
     formSubmitBtn.disabled = !loginForm.reportValidity()
     errorTextElem.textContent = ''
 }
 
-const onSubmitForm = (event) => {
-    event.preventDefault()
+const onSubmitForm = (e) => {
+    e.preventDefault()
     const userData = Object.fromEntries([...new FormData(loginForm)])
-    sendUserData(userData)
+
+    setUserData(userData)
         .then(serverResponse => serverResponse.json())
         .then(responseData => {
             alert(JSON.stringify(responseData));
             [...formInputs].forEach(inputElem => inputElem.value = '')
         })
         .catch(error => {
-            error = new Error('Failed to create user');
-            errorTextElem.textContent = error;
+            errorTextElem.textContent = 'Failed to create user'
         })
-};
+}
 
 loginForm.addEventListener('input', onInputChange)
 formSubmitBtn.addEventListener('click', onSubmitForm)
-
-const sendUserData = (userData) => {
-    return fetch(baseUrl, {
-        method: 'POST',
-        headers: headersJson,
-        body: JSON.stringify(userData)
-    })
-};
-
-//sendUserData({ email: email @email.com, firstName: "Vasyl", password: v12345v; })
+    //addNewUser({ email: email @email.com, firstName: "Vasyl", password: v12345v; })
