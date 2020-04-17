565a14a946eb1c19f734eb348ba8f56da12f1bca
nikolay07 <nikolay0701@ukr.net>
2020-04-17 17:58:04 +0300

    1

---
+++ lesson35/hw1/index.js
@@ -0,0 +1,12 @@
+const userName = '{"name" : "Nik"}';
+const parseUser = jsonUser => {
+    try {
+        const user = JSON.parse(jsonUser);
+        //console.log(user);
+        return user;
+    } catch {
+        //console.error('Error');
+        return null;
+    }
+};
+//parseUser(userName);
