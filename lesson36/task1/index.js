94d947ce5dc264b83c555504fb40dbb9cf7131a3
nikolay07 <nikolay0701@ukr.net>
2020-04-20 17:55:50 +0300

    1

---
+++ lesson36/task1/index.js
@@ -0,0 +1,17 @@
+// async function fetchUser() {
+//     const promise = new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(17);
+//         }, 1000);
+//     })
+//     const result = await promise;
+//     console.log(result);
+//     return 1;
+// };
+// const result = fetchUser()
+// console.log(result);
+
+
+const getUser = async userId => {
+    const response = await fetch(``);
+}
