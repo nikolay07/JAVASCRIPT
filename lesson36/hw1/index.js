bd31ff4490d23c9006e5884149290b5cfa74b8f4
nikolay07 <nikolay0701@ukr.net>
2020-04-20 18:50:41 +0300

    1

---
+++ lesson36/hw1/index.js
@@ -0,0 +1,12 @@
+async function getUsersBlogs(usersList) {
+    const requests = usersList
+        .map(userId => fetch(`https://api.github.com/users/${userId}`)
+            .then(response => {
+                if (response.ok) return response.json();
+                throw new Error('Failed to load data');
+            }));
+    const usersData = await Promise.all(requests);
+    return usersData.map(user => user.blog);
+};
+
+export { getUsersBlogs };
